3bb9198062b28d040a57f32dc7b822474422b300
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-28 20:52:27 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |number|
-    new_number = number ** number
+    new_number = number * number
     new_array << new_number
   end
   return new_array
